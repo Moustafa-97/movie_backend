031778249f89bafe36c20d6b6a5832a6c1d5a941
Moustafa <moustafaadel097@gmail.com>
2024-02-20 16:46:17 +0200

    movie back

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true, parameterLimit: 50000 }));
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve("../");
   app.use(express.static(path.join(__dirname + "/frontendmovieclient/build")));
-  app.get("/Popular", (req, res, next) => {
+  app.get("/*", (req, res, next) => {
     res.sendFile(
       path.resolve(__dirname, "frontendmovieclient/build", "index.html")
     );
@@ -59,26 +59,26 @@ app.use(bodyParser.json());
 // responsible to frontend connect
 // cors
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://ourmovieworld.onrender.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://ourmovieworld.onrender.com"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Private-Network", true);
+//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//   res.setHeader("Access-Control-Max-Age", 7200);
 
-  next();
-});
+//   next();
+// });
 
 app.use(
   cors({
@@ -100,11 +100,7 @@ mongoose.set("strictQuery", false);
 // mvc
 // login singup
 app.post("/signup", user_post_signup);
-app.get("/signup", (req,res)=>{
-  res.sendFile(
-    "../frontendmovieclient/build/index.html"
-  )
-});
+
 app.post("/login", user_post_login);
 
 // search
