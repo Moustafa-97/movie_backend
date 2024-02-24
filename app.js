dff73d174c01ef98d002a9fc115f22793bd23de4
Moustafa <moustafaadel097@gmail.com>
2024-02-24 19:21:58 +0200

    movie back

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const process = require("process");
 const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
 const app = express();
-// const { theUser } = require("./models/User");
 const cookieParser = require("cookie-parser");
 const server = require("http").createServer();
 const {
   user_post_signup,
   user_post_login,
 } = require("./controllers/AuthControl");
-// const io = require("socket.io")(server);
 const {
   add_remove_wishlist,
   user_search,
@@ -27,30 +25,9 @@ const {
   top_rated,
 } = require("./controllers/UserControl");
 
-app.use(express.json({ limit: "10mb" }));
+const PORT = process.env.PORT || 8000;
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, parameterLimit: 50000 }));
-
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve("../");
-  app.use(express.static(path.join(__dirname + "/frontendmovieclient/build")));
-  app.get("/*", (req, res, next) => {
-    res.sendFile(
-      path.resolve(__dirname, "frontendmovieclient/build", "index.html")
-    );
-
-    next();
-  });
-} else {
-  app.get("/", (req, res, next) => {
-    res.send("server is ready");
-    next();
-  });
-}
-
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("connected"))
-  .catch((err) => console.log(err));
 
 // Set up body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -58,6 +35,67 @@ app.use(bodyParser.json());
 
 // responsible to frontend connect
 // cors
+app.use(
+  cors({
+    origin: "https://ourmovieworld.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionSuccessStatus: 200,
+    // for cookies::
+    credentials: true,
+  })
+);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve("../");
+  app.use(express.static(path.join(__dirname + "/frontendmovieclient/build")));
+
+  // mvc
+  // login singup
+  app.post("/signup", user_post_signup);
+
+  app.post("/login", user_post_login);
+
+  // search
+  app.post("/search", user_search);
+
+  // menu wush wash
+  app.post("/Wishlist", get_wishlist_watched_elements);
+  app.post("/Watched", get_wishlist_watched_elements);
+
+  // discover page
+  app.post("/Discover", discover_page);
+  app.post("/Top", top_rated);
+
+  // home page section and popular page
+  app.post("/Popular", movie_page_popular);
+
+  // home page section
+  app.post("/Trending", trending_page);
+
+  // details
+  app.post("/Moviedetails", movie_detail);
+
+  // add and remove operation
+  app.put("/AddRemoveWatch", add_remove_watched);
+  app.put("/AddRemoveWish", add_remove_wishlist);
+
+  // series section (plan to do...)
+  // app.post("/Series", series_page);
+
+  // still not mvc, but will be in the future
+
+  app.get("*", (req, res, next) => {
+    return res.sendFile(
+      path.join(__dirname, "frontendmovieclient", "build", "index.html")
+    );
+  });
+}
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("connected"))
+  .catch((err) => console.log(err));
 
 // app.use((req, res, next) => {
 //   res.setHeader(
@@ -80,57 +118,10 @@ app.use(bodyParser.json());
 //   next();
 // });
 
-app.use(
-  cors({
-    origin: "https://ourmovieworld.onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    optionSuccessStatus: 200,
-    // for cookies::
-    credentials: true,
-  })
-);
 app.use(cookieParser());
-
-const PORT = process.env.PORT || 8000;
 
 // mongo setup
 mongoose.set("strictQuery", false);
-
-// mvc
-// login singup
-app.post("/signup", user_post_signup);
-
-app.post("/login", user_post_login);
-
-// search
-app.post("/search", user_search);
-
-// menu wush wash
-app.post("/Wishlist", get_wishlist_watched_elements);
-app.post("/Watched", get_wishlist_watched_elements);
-
-// discover page
-app.post("/Discover", discover_page);
-app.post("/Top", top_rated);
-
-// home page section and popular page
-app.post("/Popular", movie_page_popular);
-
-// home page section
-app.post("/Trending", trending_page);
-
-// details
-app.post("/Moviedetails", movie_detail);
-
-// add and remove operation
-app.put("/AddRemoveWatch", add_remove_watched);
-app.put("/AddRemoveWish", add_remove_wishlist);
-
-// series section (plan to do...)
-// app.post("/Series", series_page);
-
-// still not mvc
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
